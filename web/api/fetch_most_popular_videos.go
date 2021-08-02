9203ed5b0379f3bc86a653e2197e86d26c4ae470
sakamuuy <sakamuu.y@gmail.com>
2021-08-02 22:42:30 +0900

    implement api

--- web/api/fetch_most_popular_videos.go
+++ web/api/fetch_most_popular_videos.go
@@ -1,12 +1,37 @@
 package api
 
 import (
+	"context"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
 	"github.com/valyala/fasthttp"
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
 )
 
 func FetchMostPopularVideos() echo.HandlerFunc {
+
+	godotenv.Load()
+
 	return func(c echo.Context) error {
-		return c.JSON(fasthttp.StatusOK, "Most popular")
+		key := os.Getenv("API_KEY")
+		logrus.Debugf("KEY: %v", key)
+		context := context.Background()
+		service, err := youtube.NewService(context, option.WithAPIKey(key))
+		if err != nil {
+			logrus.Fatalf("Error creating new Youtube service: %v", err)
+		}
+
+		call := service.Videos.List([]string{"id", "snippet"}).Chart("mostPopular").MaxResults(3)
+
+		res, err := call.Do()
+		if err != nil {
+			logrus.Fatalf("Error calling Youtube API: %v", err)
+		}
+
+		return c.JSON(fasthttp.StatusOK, res)
 	}
 }
